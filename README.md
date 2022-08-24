cd8ebf6d01b214a9c07bbc1794578fe8b1c5e59f
Salva Ruiz <cuentas@salvaruiz.me>
2022-08-24 13:59:10 +0200

    Refactoring and documentation

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript_google_maps_api
+
+An interface to use Google Maps and Google Place API.
--- nearby_example.js
+++ nearby_example.js
@@ -1,22 +1,27 @@
 /**
- * @fileoverview Example of making a request to Google Places.
+ * @file An example to search for restaurants using the Google Place API.
+ * @author Salva Ruiz <salva@salvaruiz.me>
+ * @license MIT License
  */
 
-import * as GooglePlaces from "./lib/google_places.js"
+import { configSync } from "https://deno.land/std@0.152.0/dotenv/mod.ts";
+import * as GooglePlace from "./lib/google_place.js";
 
-const latitude = 36.721347
-const longitude = -4.420142
+const googleMapsApiKey = configSync().GOOGLE_MAPS_API_KEY;
 
-console.log('begin')
+const latitude = 36.721347;
+const longitude = -4.420142;
 
-GooglePlaces.nearbySearch(latitude, longitude)
-  .then(response => {
-    console.log("--- ok ---")
-    const [first] = response
-    console.log(first)
-  }).catch(message => {
-    console.log("--- error ---")
-    console.log(message)
-  })
+console.log("begin");
 
-console.log('end')
+GooglePlace.nearbySearch(latitude, longitude, googleMapsApiKey)
+  .then((response) => {
+    console.log("--- ok ---");
+    const [first] = response;
+    console.log(first);
+  }).catch((message) => {
+    console.log("--- error ---");
+    console.log(message);
+  });
+
+console.log("end");
